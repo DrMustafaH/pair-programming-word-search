85ac8d21c95394e1dc1a932e87d015c747443a98
DrMustafaH <mustafaharith@gmail.com>
2021-01-13 17:00:29 -0800

    pair program done

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) return undefined;
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const tempArr = letters[0].map((col, i) => letters.map(row => row[i]));
+    const verticalJoin = tempArr.map(ls => ls.join(''));
+    const reversedWord = word.split('').reverse().join('');
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        } else if (l.includes(reversedWord)) {
+            return true;
+        }
+        for (x of verticalJoin) {
+            if (x.includes(word)) {
+                return true;
+            } else if (x.includes(reversedWord)) {
+                return true;
+            }
+        }
     }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
